6ea46ee31cde627b1b15a6bb971b8823af6978d9
u7327620 <u7327620@anu.edu.au>
2025-06-03 10:23:51 +1000

    updated some misformatted files

--- records/util/get_all_records.py
+++ records/util/get_all_records.py
@@ -6,15 +6,8 @@ from records.util.path_finding import from_relative
 
 
 def get_tfc_history() -> dict[str, Player]:
-    all_matches: list[ToribashMatch] = []
+    all_matches: list[ToribashMatch] = get_all_matches()
     all_players: dict[str, Player] = {}
-
-    tfc_dir = from_relative("../Data/Events_Stats/TFC")
-    for folder in os.listdir(tfc_dir):
-        current_tfc_dir = os.path.join(tfc_dir, folder)
-        for filename in os.listdir(current_tfc_dir):
-            if filename.endswith(".json"):
-                all_matches.append(ToribashMatch(os.path.join(current_tfc_dir, filename)))
 
     for match in all_matches:
         if match.player1_name not in all_players.keys():
@@ -27,7 +20,7 @@ def get_tfc_history() -> dict[str, Player]:
 
     return all_players
 
-if __name__ == "__main__":
+def record_check():
     r = sorted(get_tfc_history().values(), key=lambda x: x.player_name)
     out = []
     for player in r:
@@ -36,3 +29,18 @@ if __name__ == "__main__":
 
     print(f"<Name>: <W>-<L>-<D>\n")
     print("\n".join(out))
+
+def get_all_matches() -> list[ToribashMatch]:
+    all_matches: list[ToribashMatch] = []
+    tfc_dir = from_relative("../Data/Events_Stats/TFC")
+    for folder in os.listdir(tfc_dir):
+        current_tfc_dir = os.path.join(tfc_dir, folder)
+        for filename in os.listdir(current_tfc_dir):
+            if filename.endswith(".json"):
+                all_matches.append(ToribashMatch(os.path.join(current_tfc_dir, filename)))
+    return all_matches
+
+if __name__ == "__main__":
+    players = get_tfc_history()
+    for player in sorted(players.values(), key=lambda x: len(x.matches), reverse=True):
+        print(player.player_name, player.get_win_loss())
